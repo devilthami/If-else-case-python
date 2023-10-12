7b2a1adfeeb7c3b25ad383282f8435ad29e61fc2
P@rashmgr <96672535+devilthami@users.noreply.github.com>
2023-10-12 10:38:15 +0545

    python lesson if-else-case

---
+++ For-Loop-case.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "apple":
+#     # break
+#     continue
+#     print(x)
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally Finished!")
+
+
